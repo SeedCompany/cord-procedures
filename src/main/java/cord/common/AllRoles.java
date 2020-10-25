84a457d6ce955c35271a665b99a2a2a9a4524776
Michael Marshall <michaelmarshall220@gmail.com>
2020-10-25 13:31:04 -0500

    added helper functions to all roles holder

--- src/main/java/cord/common/AllRoles.java
+++ src/main/java/cord/common/AllRoles.java
@@ -1,5 +1,8 @@
 package cord.common;
 
+import java.util.Arrays;
+import java.util.List;
+
 import cord.roles.*;
 
 public class AllRoles {
@@ -22,4 +25,55 @@ public class AllRoles {
   public static RegionalDirectorOnProject RegionalDirectorOnProject;
   public static StaffMember StaffMember;
   public static Translator Translator;
+
+  public static Object getRoleByName(String name){
+    switch (name){
+      case "Administrator":                       return AllRoles.Administrator;
+      case "Consultant":                          return AllRoles.Consultant;
+      case "ConsultantManager":                   return AllRoles.ConsultantManager;
+      case "Controller":                          return AllRoles.Controller;
+      case "FieldOperationsDirector":             return AllRoles.FieldOperationsDirector;
+      case "FinancialAnalystGlobal":              return AllRoles.FinancialAnalystGlobal;
+      case "FinancialAnalystOnProject":           return AllRoles.FinancialAnalystOnProject;
+      case "Fundraising":                         return AllRoles.Fundraising;
+      case "Intern":                              return AllRoles.Intern;
+      case "Leadership":                          return AllRoles.Leadership;
+      case "Liason":                              return AllRoles.Liason;
+      case "Marketing":                           return AllRoles.Marketing;
+      case "ProjectManagerGlobal":                return AllRoles.ProjectManagerGlobal;
+      case "ProjectManagerOnProject":             return AllRoles.ProjectManagerOnProject;
+      case "RegionalCommunicationCoordinator":    return AllRoles.RegionalCommunicationCoordinator;
+      case "RegionalDirectorGlobal":              return AllRoles.RegionalDirectorGlobal;
+      case "RegionalDirectorOnProject":           return AllRoles.RegionalDirectorOnProject;
+      case "StaffMember":                         return AllRoles.StaffMember;
+      case "Translator":                          return AllRoles.Translator;
+
+      default: return null;
+    }
+  }
+
+  public static List<String> rolesList(){
+    return Arrays.asList(
+      "Administrator",
+      "Consultant",
+      "ConsultantManager",
+      "Controller",
+      "FieldOperationsDirector",
+      "FinancialAnalystGlobal",
+      "FinancialAnalystOnProject",
+      "Fundraising",
+      "Intern",
+      "Leadership",
+      "Liason",
+      "Marketing",
+      "ProjectManagerGlobal",
+      "ProjectManagerOnProject",
+      "RegionalCommunicationCoordinator",
+      "RegionalDirectorGlobal",
+      "RegionalDirectorOnProject",
+      "StaffMember",
+      "Translator"
+    );
+  }
 }
+
