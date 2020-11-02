df92faf0d3992fdc292b32b2fc78292396e340f2
Michael Marshall <michaelmarshall220@gmail.com>
2020-11-01 19:10:42 -0600

    testing all project member roles

---
+++ src/main/java/cord/common/FeRoleNames.java
@@ -0,0 +1,20 @@
+package cord.common;
+
+public enum FeRoleNames {
+  Administrator,                 
+  Consultant,                    
+  ConsultantManager,             
+  Controller,                    
+  FieldOperationsDirector,       
+  FinancialAnalyst,               
+  Fundraising,                   
+  Intern,                        
+  Leadership,                    
+  Liason,                        
+  Marketing,                     
+  ProjectManager,                
+  RegionalCommunicationCoordinator,
+  RegionalDirector,               
+  StaffMember,                   
+  Translator,                    
+}
